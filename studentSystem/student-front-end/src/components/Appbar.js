d0490c2a07583b376ffe65131fac76643607cb1c
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2022-11-18 07:19:05 +0600

    after submit the value reset the input value

--- studentSystem/student-front-end/src/components/Appbar.js
+++ studentSystem/student-front-end/src/components/Appbar.js
@@ -7,25 +7,25 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Appbar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Spring Boot React Fulll Stack Application
-          </Typography>
-          {/* <Button color="inherit">Login</Button> */}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    >
+                    <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Spring Boot React Fulll Stack Application
+                    </Typography>
+                    {/* <Button color="inherit">Login</Button> */}
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
